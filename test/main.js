01c6d8bfbaf8619624b025fef5b406182a1a29e2
Frankie DiGiacomo <frankied3030@gmail.com>
2022-04-07 19:32:29 -0400

    Added my stuff

--- test/main.js
+++ test/main.js
@@ -1,12 +1,20 @@
-var chai    = require("chai");
+var chai = require("chai");
 var assert = chai.assert,
-    expect = chai.expect;
+  expect = chai.expect;
 var main = require("../analysis.js");
 
-describe("Sanity check", function() {
-  describe("runs", function() {
-    it("doesn't crash", function() {
-       main.complexity("analysis.js")
+describe("Sanity check", function () {
+  describe("runs", function () {
+    it("doesn't crash", function () {
+      main.complexity("analysis.js")
     });
+  });
+});
+
+describe("Strings: ", function () {
+  it("Strings in mystery.js is 19.", function () {
+    main.complexity("mystery.js")
+    let stringCount = main.getStrings();
+    assert.equal(19, stringCount);
   });
 });
